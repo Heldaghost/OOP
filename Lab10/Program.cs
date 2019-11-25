b11d54abb3a69270b501f161bd9ba1283cce906b
Heldaghost <sprudilko01@gmail.com>
2019-11-26 02:17:44 +0300

    Lab 10, Lab 11

---
+++ Lab10/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace _10lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            ArrayList arrayList = new ArrayList();
+            for (int i = 0; i < 5; ++i)
+                arrayList.Add(random.Next());
+            arrayList.Add("string");
+            arrayList.Add(new Student());
+            arrayList.RemoveAt(2);
+            Console.WriteLine($"size = {arrayList.Count}");
+            foreach (object obj in arrayList)
+                Console.WriteLine(obj);
+            Console.WriteLine($"search for string result = {arrayList.IndexOf("string")}");
+            // Generic collection
+            SortedList<long, long> genericCollection = new SortedList<long, long>();
+            for(int i = 0; i < 10; ++i)
+                genericCollection.Add(i, random.Next());
+            Console.WriteLine("Generic collection:");
+            for (int i = 1; i < 3; i++)
+                genericCollection.Remove(i);
+            foreach (KeyValuePair<long, long> item in genericCollection)
+                Console.WriteLine(item);
+            Stack<long> secondGenericCollection = new Stack<long>();
+            foreach (KeyValuePair<long, long> item in genericCollection)
+                secondGenericCollection.Push(item.Value);
+            SortedList<int, Student> ObGenericCollection = new SortedList<int, Student>();
+            for (int i = 0; i < 5; ++i)
+                ObGenericCollection.Add(i, new Student());
+            Stack < Student > ObStack = new Stack<Student>();
+            foreach (KeyValuePair<int, Student> item in ObGenericCollection)
+            {
+                Console.WriteLine(item);
+                ObStack.Push(item.Value);
+            }
+            foreach (Student item in ObStack)
+                Console.WriteLine(item);
+            ObservableCollection<int> obs = new ObservableCollection<int>();
+            obs.CollectionChanged += Ch;
+            obs.Add(5);
+            obs.Remove(5);
+            
+        }
+        public static void Ch(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            Console.WriteLine("Collection changed");
+        }
+    }
+}
---
+++ Lab11/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace OOP_Lab11
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // 1 ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+            string[] linqArray = { "January", "February", "Martch", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+
+            var lenSelected = from item in linqArray
+                                where item.Length == 4
+                                select item;
+
+            foreach (string item in lenSelected)
+                Console.WriteLine(item);
+            Console.WriteLine("Length == 4 =============================");
+
+
+
+            var sesonSelected = from item in linqArray
+                                    where item.Equals("June") || item.Equals("July") || item.Equals("August") || item.Equals("December") ||item.Equals("January") || item.Equals("February")
+                                    select item;
+
+            foreach (string item in sesonSelected)
+                Console.WriteLine(item);
+            Console.WriteLine("sesonSelected=============================");
+
+
+
+            var alphabetSelected = from item in linqArray
+                                   orderby item
+                                   select item;
+
+            foreach (string item in alphabetSelected)
+                Console.WriteLine(item);
+            Console.WriteLine("alphabetSelected=============================");
+
+
+
+            var singSelected = from item in linqArray
+                                where item.Contains("u") || item.Length > 4
+                                select item;
+
+            foreach (string item in singSelected)
+                Console.WriteLine(item);
+            Console.WriteLine("singSelected=============================");
+            // 2 ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+
+            List<Time> timeList = new List<Time>();
+            timeList.Add(new Time(5,0,0));
+            timeList.Add(new Time(13, 15));
+            timeList.Add(new Time(18, 59));
+            timeList.Add(new Time(7, 10, 38));
+
+
+            List<Time> timeList1 = new List<Time>();
+            timeList1.Add(new Time(5, 0, 0));
+            timeList1.Add(new Time(14, 15));
+            timeList1.Add(new Time(19, 59));
+            timeList1.Add(new Time(7, 10, 38));
+
+            var hourSelected = from item in timeList
+                                      where item.Hour == 5
+                                      select item;
+
+            foreach (Time item in hourSelected)
+            {
+                Console.WriteLine(timeList.IndexOf(item));
+            
+            }
+
+
+            Console.WriteLine("Hour == 5=============================");
+
+            var morningSelected = from item in timeList
+                                  where (item.Hour > 4 || item.Hour < 10)
+                                  select item;
+            foreach (Time item in morningSelected)
+            {
+                Console.WriteLine(timeList.IndexOf(item));
+
+            }
+
+            Console.WriteLine("item.Hour > 4 || item.Hour < 10=============================");
+
+            // проекция
+            var seconds = from item in timeList
+                          where item.Second != 0
+                          select item;
+
+            foreach (Time item in seconds)
+            {
+                Console.WriteLine(timeList.IndexOf(item));
+
+            }
+            Console.WriteLine("item.Second != 0=============================");
+
+            var anySelected = timeList.Any(item => item.Hour == 5);
+            Console.WriteLine(anySelected);
+
+            Console.WriteLine("Any=============================");
+
+            var allSelected = timeList.All(item => item.Minute == 30);
+            Console.WriteLine(allSelected);
+
+            Console.WriteLine("All=============================");
+
+            var averageTime = timeList.Average(item => item.Hour);
+            Console.WriteLine(averageTime);
+
+            Console.WriteLine("Average=============================");
+
+            var joinList = from item in timeList
+                           join hours in timeList1 on item.Hour equals hours.Hour
+                           select item;
+
+            foreach (Time item in joinList)
+            {
+                Console.WriteLine(timeList.IndexOf(item));
+            }
+            Console.WriteLine("Join==================================");
+        }
+    }
+}
